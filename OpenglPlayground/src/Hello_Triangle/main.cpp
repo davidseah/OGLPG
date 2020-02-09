4310964b03dec5961c9171d04c862937e3aff6b0
davidseah <ssc1987@gmail.com>
2020-02-09 20:07:33 +0800

    added shader

--- OpenglPlayground/src/Hello_Triangle/main.cpp
+++ OpenglPlayground/src/Hello_Triangle/main.cpp
@@ -5,6 +5,25 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
+const char* vertexShaderSource = 
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource =
+"#version 330 core\n"
+"out vec4 FragColor\n"
+"void main()\n"
+"{\n"
+"   FragColo = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";
+
+
+
+
 int main()
 {
 
@@ -31,6 +50,90 @@ int main()
 	}
 
 	glViewport(0, 0, 800, 600);
+
+
+
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	//Vertex Buffer Object (VBO)
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//compiling shader
+	//vertex Shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	//error checking
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if(!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	//Fragment Shader
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+	if(!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	//Shader Program
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if(!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "Shader cannot be linked " << infoLog << std::endl;
+	}
+
+	//We dont need to use the shader object once we've linked them into the program object.
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	glUseProgram(shaderProgram);
+
+
+	//The first parameter specifies which vertex attribute we want to configure. 
+	//Remember that we specified the location of the position vertex attribute 
+	//in the vertex shader with lauout (location = 0). This sets the location of the vertex attribute
+	//in the vertex attribute to 0 and since we want to pass dat to this vertex attribute, we pass in 0
+
+	//The last parameter is of type void* and this requires that weird cast. 
+	//This is the offset of where the position data begins in the buffer. 
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FLAE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	
+
+
+
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	while(!glfwWindowShouldClose(window))
